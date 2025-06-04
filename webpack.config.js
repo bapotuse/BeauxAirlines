2b8773a08001f6076045bb6fc8351c0fd0952422
bapotuse <77754709+bapotuse@users.noreply.github.com>
2025-06-04 20:51:58 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('navigation', './assets/js/navigation.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
